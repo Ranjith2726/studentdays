4d5e68ce5ff02714086079b2d38923a4995ec10d
Ranjith Dumpalapalli <ranjith2726@gmail.com>
2026-05-29 17:03:29 +0530

    Updated StudentDays website UI and authentication

--- backend/Server.js
+++ backend/Server.js
@@ -28,7 +28,7 @@ app.use("/api/offers", require("./routes/offerRoutes"));
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 // Catch-all route for React (MUST be last)
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 // ===========================================================
